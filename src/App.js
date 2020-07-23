5c9dc0676b129b806cc75b5bedbce7d7685c0578
Arkadyuti <bandyopadhyayarkadyuti@gmail.com>
2020-07-23 13:38:23 +0530

    Added form for searching movies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import SearchMovies from './components/searchMovies';
 
 function App() {
   return ( 
     <div className = "container">
       <h1 className="title">MovieDB</h1> 
+      <div className="component">
+        <SearchMovies/>
+      </div>
     </div>
   );
 }
