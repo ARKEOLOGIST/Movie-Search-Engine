276188ac6e7914390849ed868f90df0e05de4c0d
Arkadyuti <bandyopadhyayarkadyuti@gmail.com>
2020-07-23 13:03:23 +0530

    Made styling changes to the front page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return ( 
-    <div className = "App">
-      <h1> MovieDB</h1> 
+    <div className = "container">
+      <h1 className="title">MovieDB</h1> 
     </div>
   );
 }
